27a60b68b25f1b6a4a858b7630a2e89f6070aff2
lglfa <lglfa666@gmail.com>
2018-03-20 21:07:44 +0800

    Second derivate to determine the best fitting range

--- BC_Loading.py
+++ BC_Loading.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+from scipy.interpolate import UnivariateSpline
 
+################################## INPUT #########################################
 # No. of spots need to be binned and calculate k
 Spots = 5
 # No. of bin, the more the bins, the smaller the binned ATN unit
-bin_number = 30
+bin_number = 60
 # Rawdata file, following the format in the provided template "Rawdata.xlsx"
 xl_file = pd.ExcelFile("Rawdata.xlsx")
 
-################################## Main Function##################################
+################################## Main Function ##################################
 df = xl_file.parse().dropna(axis=1, how='all')
 Datetime = df.dropna().Datetime
 df.dropna()
@@ -45,25 +47,46 @@ for i in xrange(1, channels + 1):
     for j in xrange(0, tape_advances - Spots / 2 * 2):
         df_concat = pd.concat((binned_group[j:j + Spots / 2 * 2]), axis=1)
         data = df_concat.groupby(level=0, axis=1).mean()
+
+        x = data[['ATN' + str(i)]].values
+        y = data['BC' + str(i)].values
+
+        y_spl = UnivariateSpline(x, y, s=0, k=4)
+        x_range = np.linspace(x[0], x[-1], len(x))
+
+        y_spl_2d = y_spl.derivative(n=2)
+        abs_y_2d = abs(y_spl_2d(x_range))
+
+        idx = np.where(abs_y_2d <= np.nanmean(abs_y_2d))
+        idx_0 = idx[0][0]
+        idx_end = idx[0][-1]
+
         reg = linear_model.LinearRegression()
-        reg.fit(data[['ATN' + str(i)]], data['BC' + str(i)])
-        R_square = reg.score(data[['ATN' + str(i)]], data['BC' + str(i)])
+        reg.fit(x[idx_0:idx_end], y[idx_0:idx_end])
+        R_square = reg.score(x[idx_0:idx_end], y[idx_0:idx_end])
         slope = reg.coef_[0]
         intercept = reg.intercept_
         k_value = -slope / intercept
         RSq.append(R_square)
         k.append(k_value)
+
+
     RSq = [RSq[0]] * (Spots / 2) + RSq + [RSq[-1]] * (Spots / 2)
     k = [k[0]] * (Spots / 2) + k + [k[-1]] * (Spots / 2)
     R_array = np.append(R_array, RSq, axis=0)
     k_array = np.append(k_array, k, axis=0)
 
+column_R = []
+column_k = []
+for i in xrange(1,channels+1):
+    column_R.append('Rsq'+str(i))
+    column_k.append('k' + str(i))
 R_array = np.reshape(R_array, (channels, tape_advances))
 k_array = np.reshape(k_array, (channels, tape_advances))
 R_df = pd.DataFrame(R_array.T)
-R_df.columns = ['Rsq1', 'Rsq2', 'Rsq3', 'Rsq4', 'Rsq5', 'Rsq6', 'Rsq7']
+R_df.columns = column_R
 k_df = pd.DataFrame(k_array.T)
-k_df.columns = ['k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7']
+k_df.columns = column_k
 tape_advance_data = pd.concat([binTime_df, k_df, R_df], axis=1)
 tape_advance_data.to_csv('Output_k.csv', index=False)
 
